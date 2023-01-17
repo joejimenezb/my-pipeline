36fcef48ebef38a72890b041781a8e3a0957e5f1
Jofra Jimenez <jofrajim@amazon.com>
2023-01-17 13:22:24 -0600

    round 3 cicd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from my_pipeline.my_pipeline_stack import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack", 
-    env=cdk.Environment(account="111111111111", region="eu-west-1")
+    env=cdk.Environment(account="070243682791", region="us-east-1")
 )
 
 app.synth()
