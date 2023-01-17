e6e5295e2ca61e2f561888ac217152dc0a70dcae
Jofra Jimenez <jofrajim@amazon.com>
2023-01-17 13:20:48 -0600

    round 2 cicd

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -1,8 +1,9 @@
-import aws_cdk as cdk
+from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
-from constructs import Constuct
-
-class MyPipelineStack(cdk.Stack):
+from aws_cdk import (
+    Stack
+)
+class MyPipelineStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
