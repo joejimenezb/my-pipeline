fa4e4d095827b7f64b4b3e573b92dccf5870d11b
Jofra Jimenez <jofrajim@amazon.com>
2023-01-17 10:11:39 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from my_pipeline.my_pipeline_stack import MyPipelineStack
+
+
+app = cdk.App()
+MyPipelineStack(app, "MyPipelineStack",
+    env=cdk.Environment(account="070243682791", region="us-east-1")
+    )
+
+app.synth()
